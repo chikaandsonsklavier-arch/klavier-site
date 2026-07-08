b64f43f93c7a8a8cab75450d01a9f69467f125f6
chikaandsonsklavier-arch <chikaandsonsklavier@gmail.com>
2026-07-08 17:45:56 +0900

    イベント追加: ８月１４日 谷口英治(p)鈴木洋一(g)土田晴信(org)

--- js/events.js
+++ js/events.js
@@ -220,6 +220,16 @@ const KLAVIER_EVENTS = [
   },
   {
     month: 8,
+    date: "８月１４日",
+    title: "谷口英治(p)鈴木洋一(g)土田晴信(org)",
+    artist: "谷口英治(p)鈴木洋一(g)土田晴信(org)",
+    time: "OPEN 19:00 / START 19:30",
+    charge: "¥3,000",
+    description: "",
+    image: "",
+  },
+  {
+    month: 8,
     date: "８月１５日",
     title: "板垣光弘(p)大月裕信(b)関根豊明(ds)",
     artist: "板垣光弘(p)大月裕信(b)関根豊明(ds)",
