9adfbb298447634a71bacabee5662b5f671ef082
chikaandsonsklavier-arch <chikaandsonsklavier@gmail.com>
2026-08-03 12:57:54 +0900

    イベント編集: ８月２２日 河上修(b)吉永絢香(sax)

--- js/events.js
+++ js/events.js
@@ -281,7 +281,7 @@ const KLAVIER_EVENTS = [
   {
     month: 8,
     date: "８月２２日",
-    title: "河上修(b)吉永絢香(sax)",
+    title: "河上修(b)松下聖哉(p)吉永絢香(sax)",
     artist: "河上修(b)吉永絢香(sax)",
     time: "OPEN 19:00 / START 19:30",
     charge: "¥3,000",
