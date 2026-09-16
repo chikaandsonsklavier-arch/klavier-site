f4590533c6917be70472f000edf4110ef768bc31
chikaandsonsklavier-arch <chikaandsonsklavier@gmail.com>
2026-09-17 03:54:16 +0900

    イベント追加: 10月3日（土） 川嶋哲郎sax,佐津間純g &若井俊也b

--- js/events.js
+++ js/events.js
@@ -358,6 +358,16 @@ const KLAVIER_EVENTS = [
     description: "",
     image: "",
   },
+  {
+    month: 10,
+    date: "10月3日（土）",
+    title: "",
+    artist: "川嶋哲郎sax,佐津間純g &若井俊也b",
+    time: "OPEN 19:00 / START 19:30",
+    charge: "¥3,000",
+    description: "",
+    image: "",
+  },
 ];
 
 // ============================================================
